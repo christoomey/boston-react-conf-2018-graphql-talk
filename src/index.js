9a61b454971e439f2d7c7111101f69b01de31862
Chris Toomey <chris@ctoomey.com>
2018-03-09 15:50:38 -0500

    Initial react & react-dom wiring

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <p>Hello world from React!</p>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// Hot Module Replacement
+if (module.hot) {
+  module.hot.accept();
+}
