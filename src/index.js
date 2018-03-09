b14bfe69ed2fb94578115f762861742046dd6a78
Chris Toomey <chris@ctoomey.com>
2018-03-09 16:05:47 -0500

    Add prettierc

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-  <p>Hello world from React!</p>
-);
+const App = () => <p>Hello world from React!</p>;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
