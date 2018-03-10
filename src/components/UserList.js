108df089cf8465c92b1f1d80f4c834d981ec263d
Chris Toomey <chris@ctoomey.com>
2018-03-10 16:12:20 -0500

    Add pagination via fetchMore

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -3,23 +3,50 @@ import {graphql} from 'react-apollo';
 import gql from 'graphql-tag';
 import UserTile from './UserTile';
 
-const UserList = ({data: {loading, search}}) =>
+const UserList = ({data: {loading, search, fetchMore}}) =>
   loading ? (
     <p>loading...</p>
   ) : (
-    <ul>
-      {search.nodes.map(user => <UserTile key={user.login} user={user} />)}
-    </ul>
+    <div>
+      <ul>
+        {search.edges.map(({node: user}) => (
+          <UserTile key={user.login} user={user} />
+        ))}
+      </ul>
+      <button onClick={() => loadMoreResults(search.edges, fetchMore)}>
+        Load more
+      </button>
+    </div>
   );
 
+const loadMoreResults = (edges, fetchMore) => {
+  const {cursor} = edges[edges.length - 1];
+  fetchMore({
+    variables: {cursor},
+    updateQuery: (previousResult, {fetchMoreResult}) => ({
+      ...previousResult,
+      search: {
+        edges: [
+          ...previousResult.search.edges,
+          ...fetchMoreResult.search.edges,
+        ],
+      },
+    }),
+  });
+};
+
 const QUERY = gql`
-  query UserSearch($username: String!) {
-    search(first: 10, query: $username, type: USER) {
-      nodes {
-        ...UserTile
+  query UserSearch($username: String!, $cursor: String) {
+    search(first: 10, query: $username, type: USER, after: $cursor) {
+      edges {
+        cursor
+        node {
+          ...UserTile
+        }
       }
     }
   }
+
   ${UserTile.fragment}
 `;
 
