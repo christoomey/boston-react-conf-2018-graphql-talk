079e6f0e2200da23f615bef22d89bb34959d4c5a
Chris Toomey <chris@ctoomey.com>
2018-03-15 09:39:27 -0400

    Search based on the input value

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import UserList from './UserList';
 
-const SearchPage = () => (
-  <div>
-    <input type="text" />
-    <UserList username="toomey" />
-  </div>
-);
+class SearchPage extends React.Component {
+  state = {
+    login: '',
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.setState({login: event.target.querySelector("[name='login']").value});
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" name="login" />
+        </form>
+        <UserList username={this.state.login} />
+      </div>
+    );
+  }
+}
 
 export default SearchPage;
