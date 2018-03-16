b227b1777633883b3933d6ba338de3d8117a07ef
Chris Toomey <chris@ctoomey.com>
2018-03-16 14:28:16 -0400

    Add star & unstar mutation

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import gql from 'graphql-tag';
-import Language, {LANGUAGE_FRAGMENT} from './Language'
+import {graphql, compose} from 'react-apollo';
+import Language, {LANGUAGE_FRAGMENT} from './Language';
 
-const Repo = ({repo}) => (
+const Repo = ({repo, starMutation, unstarMutation}) => (
   <li key={repo.id}>
     <h4>
       <a href={repo.url}>{repo.name}</a>
     </h4>
+    <p>
+      {repo.viewerHasStarred ? (
+        <button onClick={() => unstarMutation({variables: {repoId: repo.id}})}>
+          UnStar
+        </button>
+      ) : (
+        <button onClick={() => starMutation({variables: {repoId: repo.id}})}>
+          Star
+        </button>
+      )}
+    </p>
     <p>{repo.description}</p>
     <div>
       <Language language={repo.primaryLanguage} />
@@ -35,4 +47,29 @@ export const REPO_FRAGMENT = gql`
   ${LANGUAGE_FRAGMENT}
 `;
 
-export default Repo;
+const STAR_MUTATION = gql`
+  mutation StarRepo($repoId: ID!) {
+    addStar(input: {starrableId: $repoId}) {
+      starrable {
+        ...Repo
+      }
+    }
+  }
+  ${REPO_FRAGMENT}
+`;
+
+const UNSTAR_MUTATION = gql`
+  mutation StarRepo($repoId: ID!) {
+    removeStar(input: {starrableId: $repoId}) {
+      starrable {
+        ...Repo
+      }
+    }
+  }
+  ${REPO_FRAGMENT}
+`;
+
+const withStarMutation = graphql(STAR_MUTATION, {name: 'starMutation'});
+const withUnstarMutation = graphql(UNSTAR_MUTATION, {name: 'unstarMutation'});
+
+export default compose(withStarMutation, withUnstarMutation)(Repo);
